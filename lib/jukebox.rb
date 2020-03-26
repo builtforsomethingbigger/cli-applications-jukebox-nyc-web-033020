20b03b187e91908cbd86166825d62c9c763da58b
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-26 03:40:14 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,6 +36,11 @@ def exit_jukebox
 end
 
 def run(songs)
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
   puts "Please enter a command:"
   user_response = gets.chomp
   while user_response != "exit"

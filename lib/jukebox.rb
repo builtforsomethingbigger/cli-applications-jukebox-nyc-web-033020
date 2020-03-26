ed9ecfb166f44c152899268f246f61b15ea45f40
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-26 03:11:56 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def help
-  puts "Please enter a command:"
+  puts "I accept the following commands:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
@@ -35,7 +35,7 @@ end
 def run(songs)
   help
   loop do
-    puts "I accept the following commands:"
+    puts "Please enter a command:"
     input = gets.chomp
     case input
     when 'play'

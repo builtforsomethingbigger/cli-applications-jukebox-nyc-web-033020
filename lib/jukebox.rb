57d18ca7671f957976912bcaa1cbead06d705eae
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-26 03:16:42 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -53,6 +53,8 @@ def run(songs)
         play(songs)
         puts "Please enter a command:"
         user_response = gets.chomp
+      when "exit"
+        exit_jukebox
       else
         puts "Invalid input, please try again"
         user_response = gets.chomp

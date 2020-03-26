1b4ca484affe46c00747f7da3c83b91c999ae17d
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-26 03:06:10 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,52 +1,60 @@
-require 'pry'
-
 def help
-  puts "I accept the following commands:"
-  puts "- help : displays this help message"
-  puts "- list : displays a list of songs you can play"
-  puts "- play : lets you choose a song to play"
-  puts "- exit : exits this program"
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, track|
+    puts "#{track + 1}. #{song}"
+  end
 end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  choice = gets.chomp
-  valid = false
-  songs.each_with_index do |song, index|
-    if (choice.to_i - 1 == index)
-      puts "Playing #{songs[index]}"
-      valid = true
-    elsif (choice == song)
-      puts "Playing #{choice}"
-      valid = true
-    end
-  end
-  puts "Invalid input, please try again" if valid == false
-end
+  input = gets.strip
+  number = input.to_i - 1
 
-def list(songs)
-  songs.each_with_index{|song, index| puts "#{index + 1}. #{song}"}
+  if input == "exit"
+    exit_jukebox
+  elsif number < 0 && songs.include?(input)
+    index = songs.index(input)
+    puts "Playing #{songs[index]}"
+  elsif number >= 0 && songs[number] != nil
+    puts "Playing #{songs[number]}"
+  else
+    puts "Invalid input, please try again"
+  end
 end
 
 def exit_jukebox
-  puts "Goodbye"
+  puts "Goodbye!"
 end
 
 def run(songs)
+  methods = ["help", "list", "play", "exit"]
   help
-  loop do
+  puts "Please enter a command:"
+  input = gets.strip
+
+  while !methods.include?(input)
     puts "Please enter a command:"
-    input = gets.chomp
-    case input
-    when 'play'
-      play(songs)
-    when 'help'
-      help
-    when 'list'
-      list(songs)
-    when 'exit'
-      break
-    end
+    input = gets.strip
   end
-  exit_jukebox
+
+  case input
+  when "help"
+    help
+  when "play"
+    play(songs)
+  when "list"
+    list(songs)
+  when "exit"
+    exit_jukebox
+    exit
+  end
+
+  run(songs)
 end
